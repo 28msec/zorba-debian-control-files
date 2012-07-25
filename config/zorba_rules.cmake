c060d59172aa927146350c8e047ad491cc024a40
Juan Zacarias <juan457@gmail.com>
2012-07-25 14:51:14 -0700

    Update

--- config/zorba_rules.cmake
+++ config/zorba_rules.cmake
@@ -6,13 +6,13 @@ SET(CONFIG_PACKAGE_NAME "zorba")
 SET(CONFIG_PACKAGE_VERSION "2.5.0")
 
 #set make doc command for install
-SET(CONFIG_EXTRA_INSTALL_COMMANDS "$(MAKE) -C builddir doc
-	$(MAKE) -C builddir xqjdocs")
+#SET(CONFIG_EXTRA_INSTALL_COMMANDS "$(MAKE) -C builddir doc
+#	$(MAKE) -C builddir xqjdocs")
 
 SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DZORBA_WITH_THESAURUS=ON")
 
 #Set the dependencies
-SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, swig")
+SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, swig, doxygen")
 SET(CONFIG_BINARY_DEPENDENCIES "libxml2, libicu-dev, libxerces-c3.1, libcurl3")
 
 
