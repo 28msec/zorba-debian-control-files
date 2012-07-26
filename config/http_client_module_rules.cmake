bd151e605f2fa2cad0a6817a3ab6881f01ffaadc
Juan Zacarias <juan457@gmail.com>
2012-07-26 15:43:05 -0700

    Updated

--- config/http_client_module_rules.cmake
+++ config/http_client_module_rules.cmake
@@ -4,13 +4,11 @@ SET(CONFIG_PACKAGE_NAME "zorba-http-client-module")
 #If left commented uses zorba version
 SET(CONFIG_PACKAGE_VERSION "2.5.0")
 
-SET(CONFIG_DISTRO_SERIES "precise")
-#SET(CONFIG_DISTRO_SERIES "precise" "oneiric" "natty" "lucid" "maverick")
 SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
 SET(CONFIG_SOURCE_DEPENDENCIES "zorba, zorba-html-module")
 SET(CONFIG_BINARY_DEPENDENCIES "zorba, zorba-html-module")
 
-SET(CONFIG_CMAKE_EXTRA_PARAMETER "-DSUPPRESS_HTML_MODULE_FIND_PACKAGE=ON")
+SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DSUPPRESS_HTML_MODULE_FIND_PACKAGE=ON")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
