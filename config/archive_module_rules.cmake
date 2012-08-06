ee638432402061b5de4c7d40977ddf479458cc36
Juan Zacarias <juan457@gmail.com>
2012-08-06 15:08:32 -0700

    Added archive-module's control files

---
+++ config/archive_module_rules.cmake
@@ -0,0 +1,24 @@
+#If this is left commented uses as default the name of the project 
+SET(CONFIG_PACKAGE_NAME "zorba-archive-module")
+
+#If left commented uses zorba version
+SET(CONFIG_PACKAGE_VERSION "2.6.0")
+
+SET(CONFIG_DISTRO_SERIES "precise")
+#SET(CONFIG_DISTRO_SERIES "precise" "oneiric" "natty" "lucid" "maverick")
+SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
+SET(CONFIG_SOURCE_DEPENDENCIES "zorba, libarchive-dev")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba, libarchive12")
+
+#Uncomment and set name of the file to be set as the changelog,
+#if not set default template will be used.
+#SET(CHANGELOG_FILE "ChangeLog")
+
+SET(CONFIG_SHORT_DESCRIPTION "Zorba's archive module")
+
+SET(CONFIG_DESCRIPTION 
+"This module provides functionality to work with (possibly compressed)
+ archives. For example, it provides functions to retrieve the names or
+ extract the values of several entries in a ZIP archive. Moreover,
+ there exists functions that allow to create or update archives."
+)
