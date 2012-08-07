a488608b03f78a3cd6718b039e36da976d66fea5
Juan Zacarias <juan457@gmail.com>
2012-08-07 13:18:55 -0700

    added openssl to security-module control files

--- config/security_module_rules.cmake
+++ config/security_module_rules.cmake
@@ -4,11 +4,9 @@ SET(CONFIG_PACKAGE_NAME "zorba-security-module")
 #If left commented uses zorba version
 SET(CONFIG_PACKAGE_VERSION "2.6.0")
 
-SET(CONFIG_DISTRO_SERIES "precise")
-#SET(CONFIG_DISTRO_SERIES "precise" "oneiric" "natty" "lucid" "maverick")
 SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
-SET(CONFIG_SOURCE_DEPENDENCIES "zorba")
-SET(CONFIG_BINARY_DEPENDENCIES "zorba")
+SET(CONFIG_SOURCE_DEPENDENCIES "zorba, openssl")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba, openssl")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
