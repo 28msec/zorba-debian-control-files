17cb8c0cfb1e2729f6a4de777086752598480fbd
Juan Zacarias <juan457@gmail.com>
2012-09-06 12:48:47 -0500

    Fixed email-module's binaries dependencies to work on Precise

--- config/email_module_rules.cmake
+++ config/email_module_rules.cmake
@@ -11,7 +11,7 @@ SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
 SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings, libc-client2007e-dev (>=8:2007e-1~dfsg-3.1ubuntu1zorba2), libssl-dev, libpam-krb5, libkrb5-dev, libgssapi-krb5-2, libpam-krb5, libpam-modules, libpam0g-dev, libicu-dev")
 SET(CONFIG_BINARY_DEPENDENCIES_natty "zorba, libicu44")
 SET(CONFIG_BIANRY_DEPENDENCIES_oneiric "zorba, libicu44")
-SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, libicu42")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, libicu42 | libicu48")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
