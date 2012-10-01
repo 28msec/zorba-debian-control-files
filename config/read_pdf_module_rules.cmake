eeea5b658d89709601dff07f9a35970ac1061cf5
Juan Zacarias <juan457@gmail.com>
2012-10-01 17:19:30 -0500

    Ficed dependencies in read-pdf

--- config/read_pdf_module_rules.cmake
+++ config/read_pdf_module_rules.cmake
@@ -5,8 +5,8 @@ SET(CONFIG_PACKAGE_NAME "zorba-read-pdf-module")
 SET(CONFIG_PACKAGE_VERSION "2.7.0")
 
 SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
-SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk, zorba-util-jvm-module, libpdfbox-java, libfontbox-java, libjempbox-java, libcommons-login-java")
-SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk, zorba-util-jvm-module, libpdfbox-java, libfontbox-java, libjempbox-java, libcommons-login-java")
+SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk, zorba-util-jvm-module, libpdfbox-java, libfontbox-java, libjempbox-java, libcommons-logging-java")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk, zorba-util-jvm-module, libpdfbox-java, libfontbox-java, libjempbox-java, libcommons-logging-java")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
