fbc9602794a43af0ff646a7e1c45009b1d44c208
Juan Zacarias <juan457@gmail.com>
2013-02-15 09:25:36 -0800

    Added couchbase files.

---
+++ config/couchbase_module_rules.cmake
@@ -0,0 +1,19 @@
+#If this is left commented uses as default the name of the project 
+SET(CONFIG_PACKAGE_NAME "zorba-couchbase-module")
+
+#If left commented uses zorba version
+SET(CONFIG_PACKAGE_VERSION "2.8.0")
+
+SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
+SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings | zorba-with-language-bindings, libcouchbase-dev")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba, libcouchbase-dev")
+
+#Uncomment and set name of the file to be set as the changelog,
+#if not set default template will be used.
+#SET(CHANGELOG_FILE "ChangeLog")
+
+SET(CONFIG_SHORT_DESCRIPTION "Zorba's couchbase module")
+
+SET(CONFIG_DESCRIPTION 
+""
+)
