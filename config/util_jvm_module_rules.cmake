0951068ea85a67f74214bb1f9e254bdd3100576f
Juan Zacarias <juan457@gmail.com>
2013-05-07 15:31:53 -0700

    Changed rules for util-jvm-module.

--- config/util_jvm_module_rules.cmake
+++ config/util_jvm_module_rules.cmake
@@ -7,10 +7,11 @@ SET(CONFIG_PACKAGE_VERSION "2.8.0")
 SET(CONFIG_DISTRO_SERIES "precise" "oneiric" "natty" "maverick" "lucid")
 SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
 SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk")
-SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, openjdk-6-jdk")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba | zorba-with-language-bindings, default-jre | java6-runtime | java7-runtime")
 
 SET(CONFIG_CMAKE_RPATH_OFF ON)
-SET(CONFIG_CMAKE_INSTALL_RPATH_OFF ON)
+
+SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DZORBA_ADD_DEBIAN_JRE_RPATHS=ON")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
