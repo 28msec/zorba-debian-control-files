3e28990c6ad17f73ac73523f37f930b0b4ee7df3
Juan Zacarias <juan457@gmail.com>
2013-07-05 15:13:19 -0700

    Changed ruby dependencies for zorba-with-language-bindings package.

--- config/zorba_swig_rules.cmake
+++ config/zorba_swig_rules.cmake
@@ -12,7 +12,7 @@ SET(CONFIG_PACKAGE_VERSION "2.9.1")
 SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DZORBA_WITH_THESAURUS=ON -DPHP5_EXECUTABLE=/usr/bin/php5")
 
 #Set the dependencies
-SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, ruby-dev, python-dev, php5-dev, php5, swig, doxygen, uuid-dev")
+SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, ruby1.8-dev, python-dev, php5-dev, php5, swig, doxygen, uuid-dev")
 SET(CONFIG_BINARY_DEPENDENCIES "libxml2, libicu-dev, libxerces-c3.1, libcurl3")
 
 
