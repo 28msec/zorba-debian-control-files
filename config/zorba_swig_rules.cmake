c44df66c6811981895391d3b84a97699c505438f
Juan Zacarias <juan457@gmail.com>
2013-09-18 14:21:57 -0500

    Fixed zorba-with-language-bindings building process

--- config/zorba_swig_rules.cmake
+++ config/zorba_swig_rules.cmake
@@ -9,10 +9,10 @@ SET(CONFIG_PACKAGE_VERSION "2.9.1")
 #SET(CONFIG_EXTRA_INSTALL_COMMANDS "$(MAKE) -C builddir doc
 #	$(MAKE) -C builddir xqjdocs")
 
-SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DZORBA_WITH_THESAURUS=ON -DPHP5_EXECUTABLE=/usr/bin/php5")
+SET(CONFIG_CMAKE_EXTRA_PARAMETERS "-DZORBA_WITH_THESAURUS=ON -DPHP5_EXECUTABLE=/usr/bin/php5 -DRUBY_EXECUTABLE=/usr/bin/ruby -DRUBY_INCLUDE_DIR=/usr/include/ruby-1.9.1 -DRUBY_LIBRARY=/usr/lib/libruby-1.9.1.so -DRUBY_CONFIG_INCLUDE_DIR='/usr/include/ruby-1.9.1/i686-linux/ruby:/usr/include/ruby-1.9.1/x86_64-linux/ruby' ")
 
 #Set the dependencies
-SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, ruby1.8-dev, python-dev, php5-dev, php5, swig, doxygen, uuid-dev")
+SET(CONFIG_SOURCE_DEPENDENCIES "libxml2-dev, libicu-dev, libxerces-c-dev (>=2.8.0), libcurl4-openssl-dev, openjdk-6-jdk, ruby1.9.1-dev, python-dev, php5-dev, php5, swig, doxygen, uuid-dev")
 SET(CONFIG_BINARY_DEPENDENCIES "libxml2, libicu-dev, libxerces-c3.1, libcurl3")
 
 
