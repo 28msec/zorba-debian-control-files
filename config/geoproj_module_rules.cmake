85baf7160bdcc9115502367754d485611a97fd49
Juan Zacarias <juan457@gmail.com>
2013-10-31 13:05:52 -0600

    Updated Version to 3.0

--- config/geoproj_module_rules.cmake
+++ config/geoproj_module_rules.cmake
@@ -2,7 +2,7 @@
 SET(CONFIG_PACKAGE_NAME "zorba-geoproj-module")
 
 #If left commented uses zorba version
-SET(CONFIG_PACKAGE_VERSION "2.9.1")
+SET(CONFIG_PACKAGE_VERSION "3.0")
 
 SET(CONFIG_CONTACT "Juan Zacarias <juan457@gmail.com>")
 SET(CONFIG_SOURCE_DEPENDENCIES "zorba | zorba-with-language-bindings")
