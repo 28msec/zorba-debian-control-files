82baee24db4a4f14f020e644f4c5672964e11fe1
Federico Cavalieri <f@28.io>
2015-05-22 18:11:21 +0300

    Updated website, Fixed email module dependencies

--- config/email_module_rules.cmake
+++ config/email_module_rules.cmake
@@ -6,8 +6,8 @@ SET(CONFIG_PACKAGE_NAME "zorba-email-module")
 SET(CONFIG_PACKAGE_VERSION "3.0")
 
 SET(CONFIG_CONTACT "Federico Cavalieri <f@28.io>")
-SET(CONFIG_SOURCE_DEPENDENCIES "zorba, libc-client2007e-dev (>=8:2007e-1~dfsg-3.1ubuntu1zorba2), libssl-dev, libpam-krb5, libkrb5-dev, libgssapi-krb5-2, libpam-krb5, libpam-modules, libpam0g-dev, libicu-dev")
-SET(CONFIG_BINARY_DEPENDENCIES "zorba, libicu42 | libicu48")
+SET(CONFIG_SOURCE_DEPENDENCIES "zorba, libc-client2007e-dev (= 8:2007e-1~dfsg-3.2ubuntu1zorba2) | libc-client2007e-dev (= 8:2007f-1~dfsg-2zorba1) | libc-client2007e-dev (= 8:2007f-1~dfsg-2zorba2) | libc-client2007e-dev (= 8:2007f-1~dfsg-4zorba1), libssl-dev, libpam-krb5, libkrb5-dev, libgssapi-krb5-2, libpam-krb5, libpam-modules, libpam0g-dev, libicu-dev")
+SET(CONFIG_BINARY_DEPENDENCIES "zorba, libicu48 | libicu52")
 
 #Uncomment and set name of the file to be set as the changelog,
 #if not set default template will be used.
